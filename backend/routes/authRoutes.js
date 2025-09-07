a05154a8705ff22a16039d21872294b35476f310
josh098741 <morongemokaya@gmail.com>
2025-09-08 00:49:38 +0300

    Additional commits

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+const {
+    register,
+    login,
+    logout
+} = require('../controllers/authController');
 
+const validateRegister = require('../middleware/validation/validateRegister');
+const validateLogin = require('../middleware/validation/validateLogin');
 
+//POST /api/auth/register
+router.post('/api/auth/register',validateRegister,register);
+
+//POST /api/auth/login
+router.post('/api/auth/login',validateLogin,login);
+
+//POST /api/auth/logout
+router.post('/api/auth/logout',logout)
 
 module.exports = router
