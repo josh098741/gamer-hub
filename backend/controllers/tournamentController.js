aa4e6d40e5346d511a5f1e8fc606633953818e74
josh098741 <morongemokaya@gmail.com>
2025-09-08 03:22:52 +0300

    Additional commits

---
+++ backend/controllers/tournamentController.js
@@ -0,0 +1,122 @@
+const Tournament = require("../models/Tournament");
+const User = require("../models/User");
+const Team = require("../models/Teams");
+
+
+const createTournament = async (req,res) => {
+    try{
+        const {name,type,startDate,endDate} = req.body
+        const tournament = await Tournament.create({
+            name,
+            type,
+            startDate,
+            endDate,
+            createdBy: req.user.id,
+        });
+        res.status(201).json(tournament);
+    }catch(error){
+        res.status(500).json({message: error.message});
+    }
+};
+
+
+const getTournaments = async (req,res) => {
+    try{
+        const tournaments = await Tournament.find();
+        res.json(tournaments);
+    }catch(error){
+        res.status(500).json({message: error.message})
+    }
+};
+
+
+const getTournamentsById = async (req,res) => {
+    try{
+        const tournament = await Tournament.findById(req.params.id);
+        if(!tournament){
+            return res.status(404).json({message: "Tournament not found"});
+        }
+        res.json(tournament)
+    }catch(error){
+        res.status(500).json({message: error.message});
+    }
+};
+
+
+const updateTournament = async (req,res) => {
+    try{
+        const tournament = await Tournament.findByIdAndUpdate(req.params.id, req.body ,{new:true})
+        if(!tournament){
+            return res.status(404).json({message: "Tournament not found"});
+        }
+        res.json(tournament)
+    }catch(error){
+        res.status(500).json({message: error.message})
+    }
+}
+
+
+const deleteTournament = async (req,res) => {
+    try{
+        const tournament = await Tournament.findByIdAndDelete(req.params.id);
+        if(!tournament){
+            return res.status(404).json({message: error.message})
+        }
+        res.json({message: "Tournament deleted"})
+    }catch(error){
+        res.status(500).json({message: error.message});
+    }
+};
+
+
+const registerParticipant = async(req,res) => {
+    try{
+        const tournament = await Tournament.findById(req.params.id);
+        if(!tournament){
+            return res.status(404).json({message: "Tournament not found"})
+        }
+        if(tournament.type === "solo"){
+            if(tournament.participants.includes(req.user.id)){
+                return res.status(404).json({message: "Already registered"})
+            }
+            tournament.participants.push(req.user.id);
+        }else{
+            const {teamId} = req.body;
+            if(!teamId){
+                return res.status(400).json({message: "Team Id required"});
+            }
+            if(tournament.participants.includes(teamId)){
+                return res.status(400).json({message: "team already registered"});
+            }
+            tournament.participants.push(teamId)
+        }
+        await tournament.save();
+        res.json({message: "Registration successfull", participants})
+    }catch(error){
+        res.status(500).json({message: error.message});
+    }
+};
+
+
+const getParticipant = async (req,res) => {
+    try{
+        const tournament = await Tournament.findById(req.params.id).populate("participants");
+        if(!tournament){
+            return res.status(404).json({message: "Tournament not found"});
+        }
+        res.json(tournament.participants);
+    }catch(error){
+        res.status(500).json({message: error.message});
+    }
+}
+
+
+module.exports = {
+    createTournament,
+    getTournaments,
+    getTournamentsById,
+    updateTournament,
+    deleteTournament,
+    registerParticipant,
+    getParticipant
+}
--- backend/routes/tournamentRoutes.js
+++ backend/routes/tournamentRoutes.js
@@ -1,9 +1,39 @@
 const express = require('express');
 const router = express.Router();
 
+const {
+    createTournament,
+    getTournaments,
+    getTournamentsById,
+    updateTournament,
+    deleteTournament,
+    registerParticipant,
+    getParticipants
+} = require("../controllers/tournamentController");
 
+const authMiddleware = require("../middleware/authMiddleware");
+const roleMiddleware = require("../middleware/roleMiddleware");
+const validateTournament = require("../middleware/validation/validateTournament")
 
+//CREATE tournament (admins only)
+router.post("/api/tournaments",authMiddleware,roleMiddleware(["admin"]),validateTournament,createTournament);
 
+//GET tournaments (all)
+router.get("/api/tournaments",getTournaments);
 
+//GET one tournament
+router.get("/api/tournaments/:id",getTournamentsById);
+
+//UPDATE tournament (Admin only)
+router.put("/api/tournaments/:id",authMiddleware,roleMiddleware(["admin"]),updateTournament)
+
+//DELETE tournament (Admin only)
+router.delete("/api/tournaments/:id",authMiddleware,roleMiddleware(["admin"]),deleteTournament);
+
+//REGISTER  participant (team or individual)
+router.post("/api/tournaments/:id/register",authMiddleware,registerParticipant);
+
+//GET all participants
+router.get("/api/tournaments/:id/participants",getParticipants)
 
 module.exports = router;
